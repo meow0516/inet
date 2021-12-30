c60023234f24a6c1d25264e9c53e8046e51517c0
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-30 15:46:58 +0800

    import primevue.css, primeicon, sass setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 
 createApp(App).use(PrimeVue).mount('#app')
