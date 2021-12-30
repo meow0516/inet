acb178b507589ef44db1ee322b13d5e79096423c
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-30 11:54:10 +0800

    update component structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HomePage from './components/HomePage.vue'
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HomePage msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
+
+<script setup lang="ts">
+import HomePage from './components/HomePage.vue'
+</script>
 
 <style>
 #app {
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
@@ -16,12 +8,13 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+  <p>
+    See
+    <code>README.md</code> for more information.
+  </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
@@ -32,6 +25,14 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
 
 <style scoped>
 a {
