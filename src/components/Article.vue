70b31bdb318537cfe76a363072ea52fea8bd33a1
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-30 14:33:41 +0800

    HomePage.vue: import Article Article.vue: add Article.vue

---
+++ src/components/Article.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="article-card flex justify-content-center">
+    <div class="article-info w-6 flex flex-column text-left">
+      <h3 class="article-title">
+        <a href="#">This is title</a>
+      </h3>
+      <p
+        class="article-abstract"
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum veritatis optio blanditiis ipsam minus? Dolorem aut cupiditate ex tempore ut. Earum nisi soluta odio a maxime vitae in dicta modi eveniet et quo rerum eos, veniam libero maiores repudiandae natus! Eius, rerum est qui quae ipsa at corporis nostrum. Delectus!</p>
+      <div class="author-info flex">
+        <img src="https://via.placeholder.com/50x50" alt="thumbnail" class="thumbnail" />
+        <p class="post-time">2021-10-15 發表</p>
+        <p class="browse-count">48763 次瀏覽</p>
+        <p class="like-count">87 個like</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+img {
+  border-radius: 50%;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,34 +1,12 @@
 <template>
   <NavBar />
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    See
-    <code>README.md</code> for more information.
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <Article v-for="index in 5" :key="index" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import NavBar from './NavBar.vue';
+import Article from './Article.vue'
 
 defineProps<{ msg: string }>()
 
@@ -36,19 +14,4 @@ const count = ref(0)
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
