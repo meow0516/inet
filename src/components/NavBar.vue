45443892bbbac44e424f4dae031b067652d47862
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-30 16:15:49 +0800

    NavBar.vue: update with primevue UI, and display different layout according to isLogIn

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,37 @@
 <template>
-  <nav class="flex justify-content-around">
+  <nav class="flex justify-content-between mx-6 my-3">
     <div class="menu-main flex">
-      <div class="dev">DEV</div>
-      <input type="text" class="search-bar" />
+      <div class="logo">
+        <a href="#">
+          <img src="https://via.placeholder.com/50x40" alt="logo" />
+        </a>
+      </div>
+      <div class="search mx-2">
+        <span class="p-input-icon-right">
+          <InputText type="text" class="w-20rem" />
+          <i class="pi pi-search" />
+        </span>
+      </div>
     </div>
-    <div class="menu-account flex">
-      <div class="log-in">Log in</div>
-      <div class="create-account">Create account</div>
+    <div v-if="isLogIn" class="menu-account flex">
+      <Button label="Log in" class="p-button-text" />
+      <Button label="Create account" class="p-button-outlined" />
+    </div>
+    <div v-else class="menu-account flex">
+      <Button label="Create Post" class="p-button-outlined" />
+
+      <i class="pi pi-bell text-3xl mx-2 py-2"></i>
+      <i class="pi pi-github text-3xl mx-2 py-2"></i>
     </div>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+let isLogIn = ref(false).value
 
 </script>
 
