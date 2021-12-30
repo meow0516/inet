8012ce1210a05be7952cbbd936466c7f2da112ad
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-30 11:53:48 +0800

    import PrimeVue, PrimeFlex

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
