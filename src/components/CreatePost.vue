faf7bf93e37bb3646e3ffa6581df00b9907422d1
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-03 12:07:58 +0800

    CreatePost.vue: add input area of title and content

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -1,10 +1,22 @@
 <template>
-  <NavBar />
+  <div class="flex flex-column align-items-center">
+    <div class="w-6">
+      <InputText
+        type="text"
+        class="p-inputtext-lg w-full"
+        placeholder="New Post Title Here..."
+        v-model="value"
+      />
+      <Editor v-model="value" editorStyle="height: 320px" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import NavBar from './NavBar.vue'
+import InputText from 'primevue/inputtext';
+import Editor from 'primevue/editor';
+let value: string = ''
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
