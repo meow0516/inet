4ae7c938ec92011868ec124765f2c675ec2e7801
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-03 12:07:20 +0800

    Login.vue: add input area of account and password

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,22 @@
 <template>
-  <NavBar />
+  <div class="flex justify-content-center">
+    <div class="login-info w-6 flex flex-column">
+      <div class="title">Welcome to inet Community</div>
+
+      <label for="username">Username</label>
+      <InputText id="username" type="text" v-model="value" />
+
+      <label for="password">Password</label>
+      <InputText id="password" type="password" v-model="value" />
+
+      <Button label="Submit" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import NavBar from './NavBar.vue'
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 </script>
 
 <style scoped>
