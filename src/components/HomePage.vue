69a58249eb2b2a79eace26bcfb72a95fa8536f75
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-03 16:11:54 +0800

    HomePage.vue: get articles beforeMount, update articles data source in template, import ProgressSpinner

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,33 @@
 <template>
-  <NavBar />
-  <Article v-for="index in 5" :key="index" />
+  <div class="flex mt-6" v-if="isLoading">
+    <ProgressSpinner />
+  </div>
+  <div v-else>
+    <Article v-for="(article, index) in articles" :key="article.id" :article="article" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import NavBar from './NavBar.vue';
+import { ref, reactive, onBeforeMount } from 'vue'
 import Article from './Article.vue'
+import { ArticleInfo } from '../types/ArticleInfo'
+import axios from 'axios'
 
-defineProps<{ msg: string }>()
+import ProgressSpinner from 'primevue/progressspinner';
 
-const count = ref(0)
+let isLoading = ref(true)
+let articles = reactive(<Array<ArticleInfo>>[])
+
+onBeforeMount(() => {
+  axios.get('http://172.16.240.53:45816/api/inet/articles?cursor=0&page=0&size=25').then(response => {
+    response.data.forEach((article: ArticleInfo) => {
+      articles.push(article)
+    });
+    isLoading.value = false
+  })
+})
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
