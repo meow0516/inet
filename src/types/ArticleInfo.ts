ff21552875c11f69fbd606d78e49a9b0f7ebf416
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-03 16:03:27 +0800

    add type ArticleInfo

---
+++ src/types/ArticleInfo.ts
@@ -0,0 +1,11 @@
+export type ArticleInfo = {
+    author: string,
+    body: string,
+    coverImage: string,
+    createdAt: string,
+    id: number,
+    lastModified: number,
+    likes: number,
+    title: string,
+    views: number
+  }
