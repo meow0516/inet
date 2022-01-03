886a1d01761886784b45e8336374e101e1365dce
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-03 12:10:25 +0800

    ArticleDetail.vue: add template of article title, content and comments

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,9 +1,28 @@
 <template>
-  <NavBar />
+  <div class="flex flex-column align-items-center my-2">
+    <article class="w-6 flex flex-column">
+      <div class="article-img"></div>
+      <h1 class="title">This is title</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae nisi corporis, amet similique veritatis exercitationem illum error ea quis dolorem iusto rerum omnis quaerat maxime optio velit! Illo, esse facere.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis autem optio quisquam, dolore nihil, laudantium qui similique enim quas quae tenetur. Architecto quos ab officiis vero reiciendis, assumenda adipisci amet molestias ratione similique provident dolorem esse minima totam ipsum praesentium.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae nisi corporis, amet similique veritatis exercitationem illum error ea quis dolorem iusto rerum omnis quaerat maxime optio velit! Illo, esse facere.</p>
+    </article>
+    <div class="comments w-6 flex flex-column">
+      <h2>Discussion</h2>
+      <div class="comment">
+        <div class="avatar">
+          <img src="https://via.placeholder.com/50x50" alt="avatar" />
+        </div>
+        <div class="title">comment title</div>
+        <div
+          class="body"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi labore ad facere ea temporibus reiciendis consectetur repudiandae reprehenderit accusantium tempore!</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import NavBar from './NavBar.vue'
 </script>
 
 <style scoped>
