4214f495631795193eba817eb2319a3bb38a3b1a
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-04 10:58:21 +0800

    Article.vue: link ArticleDetail with router-link

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="article-card flex justify-content-center my-2">
     <div class="w-6 flex flex-column text-left border-round border-1 border-500">
-      <div
-        class="bg-no-repeat bg-cover bg-center h-15rem"
-        :style="{ backgroundImage: 'url(' + coverImage + ')' }"
-      ></div>
-      <div class="flex px-5 pt-5">
-        <img :src="avatar" alt="avatar" class="avatar m-2" />
-        <div class="created-info flex flex-column justify-content-center">
-          <p class="author m-0">{{ author }}</p>
-          <p class="created-time m-0">{{ createdAt }} 發表</p>
+      <router-link :to="`/articles/` + id">
+        <div
+          class="bg-no-repeat bg-cover bg-center h-15rem"
+          :style="{ backgroundImage: 'url(' + coverImage + ')' }"
+        ></div>
+        <div class="flex px-5 pt-5">
+          <img :src="avatar" alt="avatar" class="avatar m-2" />
+          <div class="created-info flex flex-column justify-content-center">
+            <p class="author m-0">{{ author }}</p>
+            <p class="created-time m-0">{{ createdAt }} 發表</p>
+          </div>
         </div>
-      </div>
-      <div class="article-info px-5">
-        <h3 class="article-title font-bold">
-          <a href="#">{{ title }}</a>
-        </h3>
-        <p class="article-abstract">{{ body }}</p>
-      </div>
-      <div class="flex px-5">
-        <p class="pr-6">{{ views }} 次瀏覽</p>
-        <p>{{ likes }} 個like</p>
-      </div>
+        <div class="article-info px-5">
+          <h3 class="article-title font-bold">
+            <a href="#">{{ title }}</a>
+          </h3>
+          <p class="article-abstract">{{ body }}</p>
+        </div>
+        <div class="flex px-5">
+          <p class="pr-6">{{ views }} 次瀏覽</p>
+          <p>{{ likes }} 個like</p>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -34,7 +36,7 @@ let props = defineProps<{
 }>()
 
 let article = toRef(props, 'article')
-let { title, body, views, likes, createdAt, coverImage } = toRefs(article.value)
+let { title, body, views, likes, createdAt, coverImage, id } = toRefs(article.value)
 let author = article.value.author.name || article.value.author.username
 let avatar = article.value.author.avatar
 
