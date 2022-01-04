d34b23bd98bbcf1e7bf49ff6d2aa9fac78b52753
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-04 09:19:27 +0800

    Routes: import Login, CreatePost, ArticleDetail and set the path

--- src/router/Routes.ts
+++ src/router/Routes.ts
@@ -1,13 +1,21 @@
 import type { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import Login from '../views/Login.vue'
+import CreatePost from '../views/CreatePost.vue'
+import ArticleDetail from '../views/ArticleDetail.vue'
 
 export const RoutePath = {
     home: '/',
-    article: 'articles/:id',
+    login: '/login',
+    createpost: '/createpost',
+    article: '/articles/:id',
 };
 
 const Routes:Array<RouteRecordRaw> = [
     { name:'Home', path: RoutePath.home, component: HomePage},
+    { name:'Login', path: RoutePath.login, component: Login},
+    { name:'CreatePost', path: RoutePath.createpost, component: CreatePost},
+    { name:'article', path: RoutePath.article, component: ArticleDetail},
 ]
 
 export default Routes;
