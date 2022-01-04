6d673e84e75dc55a1c8057d4d0bba7c80da72221
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-04 17:01:29 +0800

    type AuthorInfo: update type of author name and author avatar

--- src/types/AuthorInfo.ts
+++ src/types/AuthorInfo.ts
@@ -1,6 +1,6 @@
-export type AuthorInfo = {  
-    id: number,
-    username: string,
-    name: number|string,
-    avatar: number|string
-}
+export type AuthorInfo = {
+  id: number;
+  username: string;
+  name: string | null;
+  avatar: string | null;
+};
