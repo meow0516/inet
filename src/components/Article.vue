6a7f6ddf34e8e78bffa9d3e30372d0db283c1492
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-04 12:12:38 +0800

    Article.vue: add icons of view and like, import moment and change date-time format

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -10,7 +10,7 @@
           <img :src="avatar" alt="avatar" class="avatar m-2" />
           <div class="created-info flex flex-column justify-content-center">
             <p class="author m-0">{{ author }}</p>
-            <p class="created-time m-0">{{ createdAt }} 發表</p>
+            <p class="created-time m-0">{{ convertCreatedAt }}發表</p>
           </div>
         </div>
         <div class="article-info px-5">
@@ -20,8 +20,14 @@
           <p class="article-abstract">{{ body }}</p>
         </div>
         <div class="flex px-5">
-          <p class="pr-6">{{ views }} 次瀏覽</p>
-          <p>{{ likes }} 個like</p>
+          <p class="pr-6">
+            <i class="pi pi-eye"></i>
+            {{ views }} 次瀏覽
+          </p>
+          <p>
+            <i class="pi pi-heart"></i>
+            {{ likes }} 個like
+          </p>
         </div>
       </router-link>
     </div>
@@ -31,14 +37,16 @@
 <script setup lang="ts">
 import { defineProps, toRef, toRefs } from 'vue';
 import { ArticleInfo } from '../types/ArticleInfo';
+import moment from 'moment'
+
 let props = defineProps<{
   article: ArticleInfo
 }>()
-
 let article = toRef(props, 'article')
 let { title, body, views, likes, createdAt, coverImage, id } = toRefs(article.value)
 let author = article.value.author.name || article.value.author.username
 let avatar = article.value.author.avatar
+let convertCreatedAt = moment(createdAt.value).format('YYYY-MM-DD HH:mm')
 
 </script>
 
