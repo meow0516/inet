c73f68b472353731ad4df43f3baa153ac2dcbc73
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-05 09:24:53 +0800

    apis/article: add getById

--- src/apis/article.ts
+++ src/apis/article.ts
@@ -6,8 +6,13 @@ const getByQuery = () => {
   );
 };
 
+const getById = (id: number) => {
+  return axios.get<{}>(`${process.env.API_URL}/articles/${id}`);
+};
+
 const ArticleAPI = {
   getByQuery,
+  getById,
 };
 
 export default ArticleAPI;
