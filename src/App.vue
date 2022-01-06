731074d54d6573031486b14eb08e6c6d5cd2f647
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-06 15:03:53 +0800

    App & NavBarAtCreatePost: add NavBarAtCreatePost.vue, display different NavBar at different router-link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div v-if="currentPath !== '/create-post'">
     <NavBar />
+  </div>
+  <div v-else>
+    <NavBarAtCreatePost />
   </div>
   <div class="router-view">
     <router-view></router-view>
@@ -8,7 +11,13 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import NavBar from './components/NavBar.vue'
+import NavBarAtCreatePost from './components/NavBarAtCreatePost.vue';
+
+const route = useRoute()
+let currentPath = computed(() => { return route.path })
 
 </script>
 
---
+++ src/components/NavBarAtCreatePost.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav
+    class="flex justify-content-between fixed bg-white top-0 left-0 z-1 border-bottom-1 border-200"
+  >
+    <div class="menu-main flex align-items-center">
+      <div class="logo">
+        <router-link to="/">
+          <img src="../assets/inetlogo.png" alt="logo" />
+        </router-link>
+      </div>
+      <div class="mx-2 font-bold">Create Post</div>
+    </div>
+    <div @click="confirmExit">
+      <i class="pi pi-times text-3xl mx-2 py-2"></i>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+function confirmExit() {
+  let leavePage: boolean = confirm('Do you want to leave the page?')
+  if (leavePage) {
+    router.push('/')
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+img {
+  height: 40px;
+}
+nav {
+  width: 100%;
+  padding-left: 3rem;
+  padding-right: 3rem;
+  padding-top: 0.25rem;
+  padding-bottom: 0.25rem;
+}
+@media (max-width: 768px) {
+  nav {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
+  }
+}
+</style>
