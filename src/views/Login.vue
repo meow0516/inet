d16312f60cc727d2a37192eef5de8f81f8026a8e
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-07 14:35:19 +0800

    Login.vue: update html-tag and css class in template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,21 @@
 <template>
   <form class="flex justify-content-center py-4" @submit.prevent="handleFormSubmit">
     <div class="login-info w-6 flex flex-column">
-      <div class="title">Welcome to inet Community</div>
+      <h1 class="mb-2">Welcome to inet Community</h1>
 
       <label for="username">Username</label>
-      <InputText id="username" name="username" type="text" v-model="inputUsername" />
+      <InputText id="username" name="username" type="text" v-model="inputUsername" class="mb-3" />
 
       <label for="password">Password</label>
-      <InputText id="password" name="password" type="password" v-model="inputPassword" />
+      <InputText
+        id="password"
+        name="password"
+        type="password"
+        v-model="inputPassword"
+        class="mb-3"
+      />
 
-      <Button type="submit" label="Submit" />
+      <Button type="submit" label="Log in" />
     </div>
   </form>
 </template>
