658fa81b674187b8226261e7d2377a036d012fcb
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-07 14:59:08 +0800

    import vuex

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import Router from './router';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
-import axios from 'axios'
+import axios from 'axios';
+import Store from './store';
 
-const app = createApp(App)
-app.use(Router)
-app.use(PrimeVue).mount('#app')
+const app = createApp(App);
+app.use(Router);
+app.use(Store);
+app.use(PrimeVue).mount('#app');
---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex';
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {},
+});
+export default store;
