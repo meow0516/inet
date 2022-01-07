b3915689bc9b6672fe252fcde9b1a9ccc8aa18b4
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-07 10:32:07 +0800

    apis/article: update axios response type

--- src/apis/article.ts
+++ src/apis/article.ts
@@ -1,18 +1,34 @@
 import axios from 'axios';
+import { ArticleInfo } from '../types/ArticleInfo';
 
 const getByQuery = () => {
-  return axios.get<[]>(
+  return axios.get<Array<ArticleInfo>>(
     `${process.env.API_URL}/articles?cursor=0&page=0&size=25`
   );
 };
 
 const getById = (id: number) => {
-  return axios.get<{}>(`${process.env.API_URL}/articles/${id}`);
+  return axios.get<ArticleInfo>(`${process.env.API_URL}/articles/${id}`);
+};
+
+const create = (
+  authorId: number,
+  coverImage: string,
+  title: string,
+  body: string
+) => {
+  return axios.post<ArticleInfo>(`${process.env.API_URL}/articles`, {
+    authorId: authorId,
+    coverImage: coverImage,
+    title: title,
+    body: body,
+  });
 };
 
 const ArticleAPI = {
   getByQuery,
   getById,
+  create,
 };
 
 export default ArticleAPI;
