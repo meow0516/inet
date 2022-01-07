d12f185b5f66f3c05950f6334d725c6896596b15
AzureAD\JaneChan <meow0516@gmail.com>
2022-01-07 12:21:21 +0800

    Login.vue: add function handleFormSubmit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="flex justify-content-center">
+  <form class="flex justify-content-center py-4" @submit.prevent="handleFormSubmit">
     <div class="login-info w-6 flex flex-column">
       <div class="title">Welcome to inet Community</div>
 
       <label for="username">Username</label>
-      <InputText id="username" type="text" v-model="value" />
+      <InputText id="username" name="username" type="text" v-model="inputUsername" />
 
       <label for="password">Password</label>
-      <InputText id="password" type="password" v-model="value" />
+      <InputText id="password" name="password" type="password" v-model="inputPassword" />
 
-      <Button label="Submit" />
+      <Button type="submit" label="Submit" />
     </div>
-  </div>
+  </form>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router';
+
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+
+import AuthApi from '../apis/auth';
+
+const router = useRouter()
+let inputUsername = ref('')
+let inputPassword = ref('')
+
+async function handleFormSubmit() {
+  try {
+    const { data } = await AuthApi.login({
+      username: inputUsername.value,
+      password: inputPassword.value,
+    });
+    router.push('/')
+  } catch (error: any) {
+    console.log(error.response)
+  }
+}
+
 </script>
 
 <style scoped>
